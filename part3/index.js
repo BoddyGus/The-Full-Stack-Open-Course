46c6b56292214b163bcd295231df5fec96e86b91
BoddyGus <yan.dulin06@gmail.com>
2026-08-21 15:08:29 +0300

    Completed tasks 3.5 and 3.6

--- part3/index.js
+++ part3/index.js
@@ -51,6 +51,30 @@ app.delete('/api/persons/:id', (request, response) => {
   persons = persons.filter(person => person.id !== id)
   response.status(204).end()
 })
+
+const generateId = () => {
+  return String(Math.floor(Math.random()*(10**6)))
+}
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: 'name or number missing'
+    })
+  }
+  if (persons.some(person => person.name === body.name)) {
+    return response.status(400).json({
+      error: 'name must be unique'
+    })
+  }
+  const newPerson = {
+    id: generateId(),
+    name: body.name,
+    number: body.number,
+  }
+  persons = persons.concat(newPerson)
+  response.json(newPerson)
+})
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
