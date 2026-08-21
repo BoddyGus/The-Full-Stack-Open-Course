3828849d58192320b0dd878be40f6d04116e7b0a
BoddyGus <yan.dulin06@gmail.com>
2026-08-21 15:54:21 +0300

    Done with tasks 3.7 and 3.8

--- part3/index.js
+++ part3/index.js
@@ -1,6 +1,15 @@
 const express = require('express')
+const morgan = require('morgan')
+
 const app = express()
 app.use(express.json())
+
+morgan.token('body', (request) => {
+  return JSON.stringify(request.body)
+})
+app.use(morgan(
+  ':method :url :status :res[content-length] - :response-time ms :body'
+))
 let persons = [
     {
       "id": "1",
@@ -75,6 +84,13 @@ app.post('/api/persons', (request, response) => {
   persons = persons.concat(newPerson)
   response.json(newPerson)
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
